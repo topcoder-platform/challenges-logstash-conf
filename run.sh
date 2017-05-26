16099178bfd44d5115dc8d2ad1f4395695e9889f
Guiqiang Zhang <skyhit@gmail.com>
2017-05-26 23:16:46 +0000

    update timeout

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-j2 /conf/$CONF_FILENAME-conf.j2 > $CONF_FILENAME.conf && cat $CONF_FILENAME.conf && logstash -f $CONF_FILENAME.conf >> /var/log/$CONF_FILENAME.log && tail -f /dev/null
+j2 /conf/$CONF_FILENAME-conf.j2 > $CONF_FILENAME.conf && cat $CONF_FILENAME.conf && logstash -f $CONF_FILENAME.conf
