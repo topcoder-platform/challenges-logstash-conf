e584893a5ead8a5a3fc5c10bb11f14fa25582808
Guiqiang Zhang <skyhit@gmail.com>
2017-06-12 14:14:37 +0800

    reduce the batch size

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-j2 /conf/$CONF_FILENAME-conf.j2 > $CONF_FILENAME.conf && cat $CONF_FILENAME.conf && logstash -w 1 -f $CONF_FILENAME.conf
+j2 /conf/$CONF_FILENAME-conf.j2 > $CONF_FILENAME.conf && cat $CONF_FILENAME.conf && logstash -w 1 -b 2 -f $CONF_FILENAME.conf
